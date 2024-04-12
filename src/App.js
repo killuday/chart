ca8668a663e9ca268d89ac10caa144a5a5d93569
Uday Bhanu <bhanusharm122@gmail.com>
2024-04-12 23:46:30 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React, {useRef, useState} from 'react';
+import {Line} from 'react-chartjs-2';
+import isEqual from 'lodash/isEqual'; // Import isEqual function from lodash
 import './App.css';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
 
-export default App;
+const MyChartComponent = () => {
+    const chartRef = useRef(null);
+    const [tooltip, setTooltip] = useState({
+        opacity: 0,
+        top: 0,
+        left: 0,
+        date: '',
+        value: '',
+    });
+
+    const datasets = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1,
+            },
+            {
+                label: 'Dataset 2',
+                data: [45, 89, 60, 41, 36, 75, 50],
+                fill: false,
+                borderColor: 'rgb(255, 99, 132)',
+                tension: 0.1,
+            },
+        ],
+    };
+
+
+    const options = {
+
+        plugins: {
+            tooltip: {
+                enabled: false,
+                external: context => {
+                    const tooltipModel = context.tooltip;
+                    if (!chartRef.current) return;
+
+                    if (tooltipModel.opacity === 0) {
+                        if (tooltip.opacity !== 0) setTooltip(prev => ({...prev, opacity: 0}));
+                        return;
+                    }
+
+                    const position = context.chart.canvas.getBoundingClientRect();
+                    const newTooltipData = {
+                        opacity: 1,
+                        left: position.left + tooltipModel.caretX,
+                        top: position.top + tooltipModel.caretY,
+                        date: tooltipModel.dataPoints[0].label,
+                        value: tooltipModel.dataPoints[0].formattedValue,
+                    };
+                    if (!isEqual(tooltip, newTooltipData)) setTooltip(newTooltipData);
+                },
+            },
+        },
+    };
+
+    return (
+        <>
+            <Line options={options} ref={chartRef} data={datasets}/>
+            <div className='tooltip' style={{top: tooltip.top, left: tooltip.left, opacity: tooltip.opacity}}>
+                <p style={{textAlign:'center'}}>{tooltip.date}</p>
+                <hr/>
+                <div className='data'>
+                    <p>{tooltip.date}</p>
+                    <p>{tooltip.value}</p>
+
+                </div>
+
+
+            </div>
+        </>
+    );
+};
+
+export default MyChartComponent;
